acab554e2c12f07169f71d87d164c9caff538323
soyl3y3nd4 <pacors88@gmail.com>
2022-02-08 22:22:54 +0100

    add Header and update App with BrowserRouter

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return (
-    <div>
-      <h2>Hi there!</h2>
-      <hr />
-      <MarketingApp />
-
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
