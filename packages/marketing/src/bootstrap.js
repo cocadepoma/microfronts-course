7937709de1a0720d81cdb06ee08f13b3b452b9ea
soyl3y3nd4 <pacors88@gmail.com>
2022-02-12 17:59:03 +0100

    end section 11

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      if (nextPathName !== history.location.pathname) {
+        history.push(nextPathName);
+      }
+    }
+  };
 };
 
 // If we are in development and in insolation
@@ -12,7 +27,7 @@ if (process.env.NODE_ENV === 'development') {
   // Mount the app
   const el = document.querySelector('#marketing-dev-app');
   if (el) {
-    mount(el);
+    mount(el, { defaultHistory: createBrowserHistory() });
   }
 }
 
